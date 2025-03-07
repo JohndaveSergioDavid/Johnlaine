3307e643cd7ce8cd583ecf611eac6d0a506f6eca
Johndave David (JD) <johndavesergiodavid@gmail.com>
2025-03-07 14:44:28 -0500

    Remove delete button

--- static/admin_user.js
+++ static/admin_user.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
                 "data": null,
                 "defaultContent": `
                 <div class = "btn-group">
-                <button class="btn btn-danger btn-sm delete-btn">Delete</button>
                 <button class="btn btn-primary btn-sm edit-btn">Edit</button>
                 <button class="btn btn-dark btn-sm edit-btn">Change Password</button>
                 </div>
